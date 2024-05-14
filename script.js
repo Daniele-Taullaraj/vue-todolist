8d4e13e514f0b2c80c96f4e0533d2272ea8a1bfd
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-05-14 17:50:41 +0200

    M0

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            coseDaFare: [
+                "mangiare",
+                "bere",
+                "andare a lavoro",
+                "dormire"
+            ],
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}).mount('#app')
