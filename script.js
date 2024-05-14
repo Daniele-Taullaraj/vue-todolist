78dcc5e25e092005d68d16aa5e87504e815743aa
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-05-14 21:00:08 +0200

    BONUS

--- script.js
+++ script.js
@@ -5,11 +5,21 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            // coseDaFare: [
+            //     "mangiare",
+            //     "bere",
+            //     "andare a lavoro",
+            //     "dormire"
+            // ],
             coseDaFare: [
-                "mangiare",
-                "bere",
-                "andare a lavoro",
-                "dormire"
+                { titolo: "Fare la spesa", done: false },
+                { titolo: "Studiare per l'esame", done: true },
+                { titolo: "Andare in palestra", done: false },
+                { titolo: "Pulire la casa", done: false },
+                { titolo: "Leggere un libro", done: true },
+                { titolo: "Scrivere un'email", done: false },
+                { titolo: "Fare una passeggiata", done: true },
+                { titolo: "Guardare un film", done: false }
             ],
             altroDaAggiungere: "",
         }
@@ -19,13 +29,32 @@ createApp({
             if (this.altroDaAggiungere == "") {
                 return
             } else
-                this.coseDaFare.push(this.altroDaAggiungere);
+                this.coseDaFare.push({
+                    titolo: this.altroDaAggiungere,
+                    done: false
+                });
             this.altroDaAggiungere = "";
         },
-        fatto(i) {
-            this.coseDaFare.splice(i, 1);
+        checkClick(i) {
+            // funzione per rimuovere dalla lista M2
+            // this.coseDaFare.splice(i, 1);
+            // funzione per invertire il colore BONUS
+            if (this.coseDaFare[i].done) {
+                this.coseDaFare[i].done = false;
+            } else {
+                this.coseDaFare[i].done = true;
+            }
+        },
+        controllaStato(i) {
+            if (!this.coseDaFare[i].done) {
+                return "false"
+            } else {
+                return "true"
+            }
         }
     },
     mounted() {
+
     }
+
 }).mount('#app')
